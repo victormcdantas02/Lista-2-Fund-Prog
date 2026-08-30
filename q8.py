8878b80d1cc9754d223d920c144cb971425b6343
Victor <victormcdantas@gmail.com>
2026-08-30 18:59:54 -0300

    Atividade

--- q8.py
+++ q8.py
@@ -3,6 +3,6 @@ total_clientes = 1
 for i in range(8):
     compra_cliente = float(input("Digite o valor total da compra: "))
     if compra_cliente > 50:
-            total_clientes =+ 1
+            total_clientes += 1
 
-print (f"Compras que ultrapassaram o valor:{total_clientes}")
+print (f"Compras que ultrapassaram o valor: {total_clientes}")
